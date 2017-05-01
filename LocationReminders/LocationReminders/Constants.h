ae14e4d7e0026ec46c6fc6229e78326707ba3d56
Erica Winberry <elaewin@gmail.com>
2017-05-01 11:40:53 -0700

    add file with credentials to gitignore.

---
+++ LocationReminders/LocationReminders/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  LocationReminders
+//
+//  Created by Erica Winberry on 5/1/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
